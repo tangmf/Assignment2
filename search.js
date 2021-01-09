f9aaa046b938c9828b078264416a3536cf571a06
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-01-09 16:40:29 +0800

    add feature: search based on availability+lottype

--- search.js
+++ search.js
@@ -24,6 +24,32 @@ var settings = {
   
       $("#search").empty()
       $('#search').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
+  
+  
+      var typelist = []
+      if (document.getElementById("c").checked) {
+        typelist.push("C");
+      }
+      if (document.getElementById("l").checked) {
+        typelist.push("L");
+      }
+      if (document.getElementById("y").checked) {
+        typelist.push("Y");
+      }
+      if (document.getElementById("h").checked) {
+        typelist.push("H");
+      }
+  
+      /* Availability settings */
+      var availability;
+      if (document.getElementById("all").checked){
+        availability = -1;
+      }
+      else if (document.getElementById("available").checked){
+        availability = 0;
+      }
+  
+  
       var i;
       var found = false;
       for (i = 0; i < data.value.length; i++) {
@@ -32,7 +58,7 @@ var settings = {
         let lat = location[0];
         let long = location[1];
         let str = data.value[i].Development;
-        if (str.toLowerCase().includes($("#input").val().toLowerCase())) {
+        if (str.toLowerCase().includes($("#input").val().toLowerCase()) && typelist.includes(data.value[i].LotType ) && data.value[i].AvailableLots > availability) {
           $("#search").append("<tr><td>" + data.value[i].CarParkID + "</td><td>" + data.value[i].Development + "</td><td>" + data.value[i].LotType + "</td><td>" + data.value[i].AvailableLots + "</td><td>" + long + "," + lat + "</td></tr>")
           found = true;
         }
