64af1a7d3fb5ca022f17f02f50b35ca3fca905c7
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-01-10 13:27:13 +0800

    added comments

--- parking.js
+++ parking.js
@@ -12,8 +12,6 @@ var settings = {
   /* Mapbox API token */
   mapboxgl.accessToken = 'pk.eyJ1IjoidGFuZ21pbmdmZW5nIiwiYSI6ImNrajQyazEwYzBpeWkyemxseDB6MXk2d24ifQ.I36uKUemWbTsCzrKk_SmHQ';
   
-  
-  
   /* Document Ready */
   $(document).ready(function () {
     /* Hide Loading text on ready*/
@@ -48,15 +46,12 @@ var settings = {
     $("#loading").show();
     var slider = document.getElementById("range");
     var output = document.getElementById("output");
-    output.innerHTML = slider.value + " deg"; // Display the default slider value
+    output.innerHTML = slider.value + " degrees"; // Display the default slider value
   
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function () {
-      output.innerHTML = this.value + " deg";
-  
+      output.innerHTML = this.value + " degrees";
     }
-  
-  
     $.ajax(settings).done(function (response) {
   
       let data = response
@@ -64,15 +59,9 @@ var settings = {
       $("#result").empty()
       $('#result').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
   
-      
-  
-  
-  
       /* list to store info of carparks that meet all the criteria. */
       var locationlist = []
-      
   
-      
       /* lottype list. Only checked lottypes will be displayed. When lottype is checked, it is added to typelist. In the for loop, if the carpark's lottype is in the list, and meets the other criteria, it will be outputted. */
       var typelist = []
       if (document.getElementById("c").checked) {
@@ -186,16 +175,19 @@ var settings = {
       .setLngLat(center)
       .addTo(map);
   
-  
+    /* retrieve location information to add markers and popups */
     var storedLocations = JSON.parse(localStorage.getItem("locations"));
   
     let i = 0;
+    /* loop through carparks in localstorage */
     for (i = 0; i < storedLocations.length; i++) {
       storedLocations[i] = storedLocations[i].split(" ")
+      /* Add popup */
       var popup = new mapboxgl.Popup()
         .setLngLat([storedLocations[i][1], storedLocations[i][0]])
         .setHTML(`<div id = "popup"><h3>${storedLocations[i][2]}</h3><p style=\"text-align: Left;\"><u>Location:</u>${storedLocations[i][1]},${storedLocations[i][0]}<br><u>LotType:</u> ${storedLocations[i][3]}<br><u>Available Lots:</u> ${storedLocations[i][4]}</div>`)
         .addTo(map);
+        /* Add marker */
       var marker = new mapboxgl.Marker()
         .setLngLat([storedLocations[i][1], storedLocations[i][0]])
         .addTo(map);
@@ -204,164 +196,3 @@ var settings = {
   
   }
   
-  /* Working Condition script
-  let url = "response.json";
-  
-  
-  
-  
-  function start(){
-  
-    
-    fetch(url)
-    .then(response => response.json())
-    .then(function (data) {
-      $("#result").empty()
-      $('#result').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
-  
-      var slider = document.getElementById("range");
-      var output = document.getElementById("output");
-      output.innerHTML = slider.value + " deg"; // Display the default slider value
-  
-      // Update the current slider value (each time you drag the slider handle)
-      slider.oninput = function() {
-        output.innerHTML = this.value + " deg";
-        start();
-  
-      }
-      var locationlist = []
-      var typelist = []
-  
-      var i;
-      var found = false;
-  
-      if (document.getElementById("c").checked){
-          typelist.push("C");
-        }
-        if (document.getElementById("l").checked){
-          typelist.push("L");
-        }
-        if (document.getElementById("y").checked){
-          typelist.push("Y");
-        }
-        if (document.getElementById("h").checked){
-          typelist.push("H");
-        }
-      for(i=0;i<data.value.length;i++){
-        let location = data.value[i].Location
-        location = location.split(" ");
-        let lat = location[0];
-        let long = location[1];
-  
-        let range = output.innerHTML;
-        let deg = range.split(" ")[0]/100;
-  
-        
-        
-        
-        var lottype = document.getElementById("lottype-settings");
-        lottype.oninput = function() {
-          start();
-          
-  
-        
-        
-        }
-        
-        
-        if (Math.abs(lat - parseFloat($("#currentlat").text())) <= deg && Math.abs(long - parseFloat($("#currentlong").text())) <= deg && typelist.includes(data.value[i].LotType)) {
-          
-          $("#result").append("<tr><td>" + data.value[i].CarParkID + "</td><td>" + data.value[i].Development + "</td><td>" + data.value[i].LotType + "</td><td>" + data.value[i].AvailableLots + "</td><td>" + long +"," + lat + "</td></tr>")
-          found = true;
-  
-  
-          locationlist.push(data.value[i].Location + " " + data.value[i].CarParkID + " " + data.value[i].LotType + " " + data.value[i].AvailableLots)
-          
-  
-          
-          
-        }
-        
-  
-        
-  
-      }
-      if (found == false){
-          $("#result").append("<tr><td>No carparks nearby</td></tr>")
-          
-        }
-  
-    var typelist = []
-    
-    
-    localStorage.setItem("locations", JSON.stringify(locationlist));
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation,
-    {
-      enableHighAccuracy: true
-    })
-  
-  })
-  
-  }
-  
-  
-  mapboxgl.accessToken = 'pk.eyJ1IjoidGFuZ21pbmdmZW5nIiwiYSI6ImNrajQyazEwYzBpeWkyemxseDB6MXk2d24ifQ.I36uKUemWbTsCzrKk_SmHQ';
-  
-  start();
-  navigator.geolocation.getCurrentPosition(successLocation, errorLocation,
-    {
-      enableHighAccuracy: true
-    })
-  
-  function successLocation(position) {
-    setupMap([position.coords.longitude, position.coords.latitude]);
-    
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-  
-    $("#currentlat").text(`${lat}`);
-    $("#currentlong").text(`${long}`);
-  
-  
-  }
-  
-  function errorLocation() { }
-  
-  function setupMap(center) {
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: center,
-      zoom: 15
-    })
-    const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav);
-  
-    var directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken
-    })
-  
-    map.addControl(directions, 'top-left');
-  
-    var marker = new mapboxgl.Marker({color: 'red'})
-  .setLngLat(center)
-  .addTo(map);
-  
-  
-  var storedLocations = JSON.parse(localStorage.getItem("locations"));
-  
-  let i = 0;
-  for (i=0;i<storedLocations.length;i++){
-    storedLocations[i] = storedLocations[i].split(" ")
-    var popup = new mapboxgl.Popup()
-    .setLngLat([storedLocations[i][1],storedLocations[i][0]])
-    .setHTML(`<h3>${storedLocations[i][2]}</h3><p style=\"text-align: Left;\"><u>Location:</u>${storedLocations[i][1]},${storedLocations[i][0]}<br><u>LotType:</u> ${storedLocations[i][3]}<br><u>Available Lots:</u> ${storedLocations[i][4]}`)
-    .addTo(map);
-    var marker = new mapboxgl.Marker()
-  .setLngLat([storedLocations[i][1],storedLocations[i][0]])
-  .addTo(map);
-  }
-  
-  
-  }
-  */
--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+/* settings for LTA DataMall API */
 var settings = {
     "url": "https://cors-anywhere.herokuapp.com/http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2?",
     "method": "GET",
@@ -8,30 +9,38 @@ var settings = {
     },
   };
   
+  /* Document ready */
   $(document).ready(function () {
+    /* Hide loading text and options dropdown */
     $("#loading").hide();
     $("#settings-bar").hide();
   
+    /* Dropdown toggle button */
     $("#form-toggle").click(function () {
         $("#settings-bar").toggle();
       })
   
+    /* Search button */
     search();
-    $("#btn").click(function () {
+    $("#search").click(function () {
       search();
     });
   })
   
+  /* Search for carparks based on settings/options selected by user */
   function search() {
+    /* show loading text */
     $("#loading").show();
+  
+    /* Read from LTA DataMall API */
     $.ajax(settings).done(function (response) {
   
       let data = response
+      /* Reset table */
+      $("#result").empty()
+      $('#result').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
   
-      $("#search").empty()
-      $('#search').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
-  
-  
+      /* List for the lot types. Only those in the list will be displayed*/
       var typelist = []
       if (document.getElementById("c").checked) {
         typelist.push("C");
@@ -55,62 +64,31 @@ var settings = {
         availability = 0;
       }
   
-  
+      /* Loop through all carparks in the API */
       var i;
       var found = false;
       for (i = 0; i < data.value.length; i++) {
+        /* Split location into long and lat */
         let location = data.value[i].Location
         location = location.split(" ");
         let lat = location[0];
         let long = location[1];
         let str = data.value[i].Development;
+        /* Make it so that the search function is not case sensitive */
         if (str.toLowerCase().includes($("#input").val().toLowerCase()) && typelist.includes(data.value[i].LotType ) && data.value[i].AvailableLots > availability) {
-          $("#search").append("<tr><td>" + data.value[i].CarParkID + "</td><td>" + data.value[i].Development + "</td><td>" + data.value[i].LotType + "</td><td>" + data.value[i].AvailableLots + "</td><td>" + long + "," + lat + "</td></tr>")
+          /* append to table */
+          $("#result").append("<tr><td>" + data.value[i].CarParkID + "</td><td>" + data.value[i].Development + "</td><td>" + data.value[i].LotType + "</td><td>" + data.value[i].AvailableLots + "</td><td>" + long + "," + lat + "</td></tr>")
           found = true;
         }
       }
+      /* When there is no output (empty table), the user is informed. */
       if (found == false) {
-        $("#search").append("<tr><td>No results</td></tr>")
+        $("#result").append("<tr><td>No results</td></tr>")
       }
+      /* loading is finished, and output is successfully displayed */
     $("#loading").hide();
     })
     
   }
-  
-  /* Using response.json
-  let url = "response.json";
-  $(document).ready(function(){
-    searchbyarea();
-    $("#btn").click(function(){
-      searchbyarea();
-    });
-  })
-  
-  function searchbyarea() {
-    fetch(url)
-      .then(response => response.json())
-      .then(function (data) {
-        var locationlist = []
-        $("#search").empty()
-        $('#search').append('<tr><th>Car Park ID</th><th>Development</th><th>Lot type</th><th>Available lots</th><th>Location</th></tr>')
-        var i;
-        var found = false;
-        for(i=0;i<data.value.length;i++){
-          let location = data.value[i].Location
-          location = location.split(" ");
-          let lat = location[0];
-          let long = location[1];
-          let str = data.value[i].Development;
-          if (str.toLowerCase().includes($("#input").val().toLowerCase())) {
-            $("#search").append("<tr><td>" + data.value[i].CarParkID + "</td><td>" + data.value[i].Development + "</td><td>" + data.value[i].LotType + "</td><td>" + data.value[i].AvailableLots + "</td><td>" + long +"," + lat + "</td></tr>")
-            found = true;
-          }
-        }
-        if (found == false){
-          $("#search").append("<tr><td>No results</td></tr>")       
-        }
-      });
-  }
-  */
   
   
